32ad961fe2b13c5d204f05058da57b61a4fde134
Don van Gent <don.v.gent@northwave-cybersecurity.com>
2026-06-19 23:58:45 +0200

    cmd: set version to 0.3

--- cmd/swimpeek/swimpeek.go
+++ cmd/swimpeek/swimpeek.go
@@ -18,7 +18,7 @@ import (
 )
 
 var logger *log.Logger = config.GetLogger("swimpeek")
-var version string = "0.2"
+var version string = "0.3"
 
 func printUsage() {
 	fmt.Println("Usage: swimpeek <command> [options]")
